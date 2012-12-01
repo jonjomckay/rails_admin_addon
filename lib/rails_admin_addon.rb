2e7b1b19201f19aaaa938b0da8374765baf1823e
Jonjo McKay <jonjo@jonjomckay.com>
2012-12-01 02:24:27 +0000

    Added form_addon view and registered instance options

--- lib/rails_admin_addon.rb
+++ lib/rails_admin_addon.rb
@@ -12,6 +12,25 @@ module RailsAdmin
       module Types
         class Addon < RailsAdmin::Config::Fields::Base
           RailsAdmin::Config::Fields::Types::register(self)
+
+          register_instance_option :html_attributes do
+            {
+                :maxlength => 50,
+                :size => [50, length.to_i].min
+            }
+          end
+
+          register_instance_option :prepend_text do
+
+          end
+
+          register_instance_option :append_text do
+
+          end
+
+          register_instance_option :partial do
+            :form_addon
+          end
         end
       end
     end
